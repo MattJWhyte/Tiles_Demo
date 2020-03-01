03043559a761b95a1332f88e1576d9498545cb6f
MattJWhyte <47209554+MattJWhyte@users.noreply.github.com>
2020-03-01 13:10:05 +0000

    Add files via upload

---
+++ PointCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCount : MonoBehaviour
+{
+    public int totalScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Point")
+        {
+            Destroy(collision.gameObject);
+            totalScore += 1;
+        }
+    }
+}
